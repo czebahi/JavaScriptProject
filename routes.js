bd361caaa0c428fe8baddd86a6654942ab77d34b
Hongjo Lim <hongjo1988@gmail.com>
2019-06-29 15:06:38 -0400

    added login, register, logout function etc

--- routes.js
+++ routes.js
@@ -6,11 +6,13 @@ const app = express();
 const pagesRoutes = require('./routes/pages');
 const moviesRoutes = require('./routes/movies');
 const sessionsRoutes = require('./routes/sessions');
+const authorRoutes = require('./routes/authors');
 
 // Registering our pageRoutes
 app.use('/', pagesRoutes);
 app.use('/movies', moviesRoutes);
 app.use('/', sessionsRoutes);
+app.use('/authors', authorRoutes);
 
 // Exporting the changes
 module.exports = app;
