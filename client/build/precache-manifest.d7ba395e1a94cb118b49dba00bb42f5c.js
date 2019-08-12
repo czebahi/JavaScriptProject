c795ca9bfac9917829c89c45840654816adac9e0
Jo Lim <35909587+czebahi@users.noreply.github.com>
2019-08-11 22:18:01 -0400

    Add files via upload

---
+++ client/build/precache-manifest.d7ba395e1a94cb118b49dba00bb42f5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6faf91e88b419cd34e7e6d5b4b39e20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ded3cf1550fd00104791",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ded3cf1550fd00104791",
+    "url": "/static/js/2.ddce1204.chunk.js"
+  },
+  {
+    "revision": "0b6c607b60429b41578e",
+    "url": "/static/js/main.329ae1e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
